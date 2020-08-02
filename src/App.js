639057ccebf0bd98bb22dcae3412ddcce7cbd21e
LawrenceObote <lawrencerobote@gmail.com>
2020-08-02 00:06:52 -0400

    Added a tags to projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './components/About';
-import OpengraphReactComponent from 'opengraph-react';
-import Project from './components/Projects';
+import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Navbar from './components/Navbar';
-import { Parallax, Background } from 'react-parallax';
+import { Parallax } from 'react-parallax';
 
 
 function App() {
@@ -21,8 +19,8 @@ function App() {
                             bgImage={'/assets/RedLightBulb.jpg'}
                             bgImageAlt="Red Light Bulb"
                             strength={500}>
-                            <div style={{ height: '50vh', width: '600px' }}></div></Parallax>
-                        <Project></Project>
+                            <div style={{ height: '100vh', width: '600px' }}></div></Parallax>
+                        <Projects></Projects>
                         <Contact></Contact>
                     </Route>
                 </Switch>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,25 +6,43 @@ export default function Projects() {
         <div className="projects">
             <h1 className="title">Projects</h1>
             <div className="thumbnail-div">
-                <div className="thumbnail-pair">
-                    <Row>
-                        <Col md="6" className="left">
+                <Row className="two-thumbnails">
+                    <Col md="6" className="left">
+                        <a href="https://gracious-roentgen-044e5d.netlify.app/">
                             <div className="bar">
                                 <p className="bar-text">Anime App</p>
                             </div>
-                            <img
+                            <img src="/assets/AnimeAppThumbnailCrop.jpg" alt={"Anime Application"}
                                 className="thumbnail"
-                                src="/assets/AnimeAppThumbnailCrop.jpg"
                             ></img>
-                        </Col>
-                        <Col md="6" className="right">
+                        </a>
+                    </Col>
+                    <Col md="6" className="right">
+                        <a href="https://agitated-babbage-914960.netlify.app/">
                             <div className="bar">
                                 <p className="bar-text">Tap of War</p>
                             </div>
                             <img
                                 className="thumbnail"
                                 src="/assets/TapOfWarThumbnailCrop.jpg"
+                                alt={"Tap of War game"}
                             ></img>
+                        </a>
+                    </Col>
+                </Row>
+                <div className="thumbnail-single">
+                    <Row>
+                        <Col md="6" className="center">
+                            <a href="https://jolly-nobel-a5c623.netlify.app/">
+                                <div className="bar">
+                                    <p className="bar-text">Cupcake Shop</p>
+                                </div>
+                                <img
+                                    className="thumbnail"
+                                    src="/assets/cupcakeShop.jpg"
+                                    alt="Cupcake Shop"
+                                ></img>
+                            </a>
                         </Col>
                     </Row>
                 </div>
