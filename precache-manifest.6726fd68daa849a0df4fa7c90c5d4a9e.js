cb8c62551148da189208374dc2140c560b0d0b44
LawrenceObote <lawrencerobote@gmail.com>
2020-08-02 00:18:55 -0400

    Updates

---
+++ precache-manifest.6726fd68daa849a0df4fa7c90c5d4a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1589473a1a2638acdb0ba89136b34eea",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ff845a4301bba1203960",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6b6d062ae95423e2a826",
+    "url": "/Portfolio/static/css/main.e5fefa1b.chunk.css"
+  },
+  {
+    "revision": "ff845a4301bba1203960",
+    "url": "/Portfolio/static/js/2.9f9696e2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.9f9696e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6d062ae95423e2a826",
+    "url": "/Portfolio/static/js/main.c848be6e.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
